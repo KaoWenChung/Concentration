45e7602bc0e75393bf8648df4ca792726c38b163
KaoWenChung <k022298@gmail.com>
2020-12-08 15:27:33 +0800

    Finish the card UI and filpping method

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🐶", on: sender)
+
     }
 
+    private func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 
 }
 
