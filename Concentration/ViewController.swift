cd5ec0755e80db3ca345f6ffc76a102dd0aef224
KaoWenChung <k022298@gmail.com>
2020-12-08 16:54:03 +0800

    Setup flip count and four cards with emoji array

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -8,16 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var flipCount: Int = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: " + flipCount.description
+        }
+    }
+
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet var flipCountLabel: UILabel!
+    private let emojiChoices = ["🐱","🐶","🐱","🐶"]
 
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "🐶", on: sender)
-
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Chosen card was not in cardButtons")
+        }
     }
 
     private func flipCard(withEmoji emoji: String, on button: UIButton) {
         if button.currentTitle == emoji {
             button.setTitle("", for: .normal)
-            button.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         } else {
             button.setTitle(emoji, for: .normal)
             button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
